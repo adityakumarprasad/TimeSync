ea8e46576752b2dd91470e86a53c37f13531655b
itsad <itsad@users.noreply.github.com>
2026-06-03 16:05:46 +0530

    setup tailwind and shadcn

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
